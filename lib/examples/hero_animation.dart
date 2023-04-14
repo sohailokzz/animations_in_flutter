54e26e5bda29e57bcd7f39dba96a19bdf4162edf
Sohail Orakzai <sohailokzz@gmail.com>
2023-04-15 02:16:13 +0500

    Hero Animation

--- lib/examples/hero_animation.dart
+++ lib/examples/hero_animation.dart
@@ -12,6 +12,146 @@ class HerooAnimation extends StatefulWidget {
 class _HerooAnimationState extends State<HerooAnimation> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('People'),
+        centerTitle: true,
+      ),
+      body: ListView.builder(
+        itemCount: people.length,
+        itemBuilder: (context, index) {
+          final person = people[index];
+          return ListTile(
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) {
+                    return DetailsScreen(
+                      person: person,
+                    );
+                  },
+                ),
+              );
+            },
+            leading: Hero(
+              tag: person.name,
+              child: Text(
+                person.emoji,
+                style: const TextStyle(fontSize: 40),
+              ),
+            ),
+            title: Text(person.name),
+            subtitle: Text('${person.age} year old'),
+            trailing: const Icon(
+              Icons.arrow_forward_ios,
+            ),
+          );
+        },
+      ),
+    );
   }
+}
+
+class DetailsScreen extends StatelessWidget {
+  final Person person;
+  const DetailsScreen({
+    super.key,
+    required this.person,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Hero(
+          flightShuttleBuilder: (
+            flightContext,
+            animation,
+            flightDirection,
+            fromHeroContext,
+            toHeroContext,
+          ) {
+            switch (flightDirection) {
+              case HeroFlightDirection.push:
+                return Material(
+                  color: Colors.transparent,
+                  child: ScaleTransition(
+                    scale: animation.drive(
+                      Tween<double>(
+                        begin: 0.0,
+                        end: 1.0,
+                      ).chain(
+                        CurveTween(
+                          curve: Curves.fastLinearToSlowEaseIn,
+                        ),
+                      ),
+                    ),
+                    child: toHeroContext.widget,
+                  ),
+                );
+
+              case HeroFlightDirection.pop:
+                return Material(
+                  color: Colors.transparent,
+                  child: fromHeroContext.widget,
+                );
+            }
+          },
+          tag: person.name,
+          child: Text(
+            person.emoji,
+            style: const TextStyle(
+              fontSize: 40,
+            ),
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              person.name,
+              style: const TextStyle(
+                fontSize: 20,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Text(
+              '${person.age} years old',
+              style: const TextStyle(
+                fontSize: 20,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+const people = [
+  Person(name: 'Khan', age: 20, emoji: '🙋‍♂️'),
+  Person(name: 'Ali', age: 24, emoji: '🙆‍♂️'),
+  Person(name: 'Aslam', age: 22, emoji: '👨'),
+  Person(name: 'Ahmad', age: 18, emoji: '👲'),
+  Person(name: 'Shan', age: 23, emoji: '👷‍♂️'),
+];
+
+@immutable
+class Person {
+  final String name;
+  final int age;
+  final String emoji;
+
+  const Person({
+    required this.name,
+    required this.age,
+    required this.emoji,
+  });
 }
