1a6dfbe6b036c6d5ba04a1a41c0412a9985330c6
Sohail Orakzai <sohailokzz@gmail.com>
2023-04-22 00:55:23 +0500

    Chained Animation Completed

--- lib/examples/chained_animation.dart
+++ lib/examples/chained_animation.dart
@@ -1,3 +1,4 @@
+import 'dart:math' show pi;
 import 'package:flutter/material.dart';
 
 class ChainedAnimation extends StatefulWidget {
@@ -7,38 +8,171 @@ class ChainedAnimation extends StatefulWidget {
   State<ChainedAnimation> createState() => _ChainedAnimationState();
 }
 
-class _ChainedAnimationState extends State<ChainedAnimation> {
+class _ChainedAnimationState extends State<ChainedAnimation>
+    with TickerProviderStateMixin {
+  late AnimationController _counterClockwiseRotationAnimationController;
+  late Animation<double> _counterClockwiseRotationAnimation;
+  late AnimationController _flipAnimationController;
+  late Animation<double> _flipAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _counterClockwiseRotationAnimationController = AnimationController(
+      vsync: this,
+      duration: const Duration(
+        seconds: 1,
+      ),
+    );
+
+    _counterClockwiseRotationAnimation = Tween<double>(
+      begin: 0,
+      end: -(pi / 2),
+    ).animate(
+      CurvedAnimation(
+        parent: _counterClockwiseRotationAnimationController,
+        curve: Curves.bounceOut,
+      ),
+    );
+
+    _flipAnimationController = AnimationController(
+      vsync: this,
+      duration: const Duration(
+        seconds: 1,
+      ),
+    );
+
+    _flipAnimation = Tween<double>(
+      begin: 0,
+      end: pi,
+    ).animate(
+      CurvedAnimation(
+        parent: _flipAnimationController,
+        curve: Curves.bounceOut,
+      ),
+    );
+
+    _counterClockwiseRotationAnimationController.addStatusListener(
+      (status) {
+        if (status == AnimationStatus.completed) {
+          _flipAnimation = Tween<double>(
+            begin: _flipAnimation.value,
+            end: _flipAnimation.value + pi,
+          ).animate(
+            CurvedAnimation(
+              parent: _flipAnimationController,
+              curve: Curves.bounceOut,
+            ),
+          );
+
+          _flipAnimationController
+            ..reset()
+            ..forward();
+        }
+      },
+    );
+
+    _flipAnimationController.addStatusListener(
+      (status) {
+        if (status == AnimationStatus.completed) {
+          _counterClockwiseRotationAnimation = Tween<double>(
+            begin: _counterClockwiseRotationAnimation.value,
+            end: _counterClockwiseRotationAnimation.value + -(pi / 2),
+          ).animate(
+            CurvedAnimation(
+              parent: _counterClockwiseRotationAnimationController,
+              curve: Curves.bounceOut,
+            ),
+          );
+
+          _counterClockwiseRotationAnimationController
+            ..reset()
+            ..forward();
+        }
+      },
+    );
+  }
+
+  @override
+  void dispose() {
+    _counterClockwiseRotationAnimationController.dispose();
+    _flipAnimationController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    Future.delayed(const Duration(seconds: 1), () {
+      _counterClockwiseRotationAnimationController
+        ..reset()
+        ..forward();
+    });
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Chained Animation'),
       ),
       body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ClipPath(
-              clipper: HalfCircleClipper(
-                circleSides: CircleSides.left,
+        child: AnimatedBuilder(
+          animation: _counterClockwiseRotationAnimationController,
+          builder: (context, child) {
+            return Transform(
+              alignment: Alignment.center,
+              transform: Matrix4.identity()
+                ..rotateZ(
+                  _counterClockwiseRotationAnimation.value,
+                ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  AnimatedBuilder(
+                    animation: _flipAnimationController,
+                    builder: (context, child) {
+                      return Transform(
+                        alignment: Alignment.centerRight,
+                        transform: Matrix4.identity()
+                          ..rotateY(
+                            _flipAnimation.value,
+                          ),
+                        child: ClipPath(
+                          clipper: HalfCircleClipper(
+                            circleSides: CircleSides.left,
+                          ),
+                          child: Container(
+                            color: const Color(0xff0057b7),
+                            width: 150,
+                            height: 150,
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                  AnimatedBuilder(
+                    animation: _flipAnimationController,
+                    builder: (context, child) {
+                      return Transform(
+                        alignment: Alignment.centerLeft,
+                        transform: Matrix4.identity()
+                          ..rotateY(
+                            _flipAnimation.value,
+                          ),
+                        child: ClipPath(
+                          clipper: HalfCircleClipper(
+                            circleSides: CircleSides.right,
+                          ),
+                          child: Container(
+                            color: const Color(0xffffd700),
+                            width: 150,
+                            height: 150,
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ],
               ),
-              child: Container(
-                color: const Color(0xff0057b7),
-                width: 150,
-                height: 150,
-              ),
-            ),
-            ClipPath(
-              clipper: HalfCircleClipper(
-                circleSides: CircleSides.right,
-              ),
-              child: Container(
-                color: const Color(0xffffd700),
-                width: 150,
-                height: 150,
-              ),
-            ),
-          ],
+            );
+          },
         ),
       ),
     );
